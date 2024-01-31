f83ed93a23a35e27f713e8b406a7fbd47d742718
Cedar Drake <cedar.drake@mrjaffesclass.com>
2024-01-31 11:44:11 -0800

    Clock first day!

---
+++ Clock/Clock/Constants.java
@@ -0,0 +1,15 @@
+package Clock;
+
+
+/**
+ * Write a description of class Constants here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Constants
+{
+    public static final int DECK_SIZE = 52;
+    public static final int SUIT_SIZE = 13;
+    public static final int MAX_HAND = 4;
+}
